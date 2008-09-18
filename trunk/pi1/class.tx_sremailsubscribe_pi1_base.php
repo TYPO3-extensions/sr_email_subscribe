b8b4721800aeea3b0b18f008da02ba005626a01d
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-18 14:46:26 +0000

    add $conf to pibase object
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe@12097 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/pi1/class.tx_sremailsubscribe_pi1_base.php
@@ -0,0 +1,94 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 1999-2003 Kasper Skårhøj <kasperYYYY@typo3.com>
+*  (c) 2004-2008 Stanislas Rolland <stanislas.rolland(arobas)sjbr.ca>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ *
+ * Front End creating/editing/deleting records authenticated by email address, also called subscriptions.
+ *
+ * $Id: class.tx_sremailsubscribe_pi1.php 9356 2008-06-19 18:10:15Z franzholz $
+ *
+ * @author	Kasper Skårhøj <kasperYYYY@typo3.com>
+ * @author	Stanislas Rolland <stanislas.rolland(arobas)sjbr.ca>
+ * @author	Franz Holzinger <contact@fholzinger.com>
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_BE_srfeuserregister.'control/class.tx_srfeuserregister_control_main.php');
+
+class tx_sremailsubscribe_pi1_base extends tslib_pibase {
+
+		// Plugin initialization
+	var $prefixId = 'tx_sremailsubscribe_pi1';  // Same as class name
+	var $scriptRelPath = 'pi1/class.tx_sremailsubscribe_pi1.php'; // Path to this script relative to the extension dir.
+	var $extKey = SR_EMAIL_SUBSCRIBE_EXTkey;  // The extension key.
+	var $adminFieldList = 'name,hidden';
+
+	function main($content, &$conf) {
+		global $TSFE;
+
+		$this->conf = &$conf;
+		$this->pi_setPiVarDefaults();
+
+		$adminFieldList = 'name,hidden';
+		$buttonLabelsList = 'register,confirm_register,send_invitation,send_invitation_now,send_link,back_to_form,update,confirm_update,enter,confirm_delete,cancel_delete';
+		$otherLabelsList = 'yes,no,click_here_to_register,tooltip_click_here_to_register,v_already_subscribed,click_here_to_edit,tooltip_click_here_to_edit,
+		v_wish_to_update_or_delete,v_enter_subscribed_email,click_here_to_delete,tooltip_click_here_to_delete,
+		copy_paste_link,enter_account_info,enter_invitation_account_info,required_info_notice,excuse_us,excuse_us_invitation,
+		registration_problem,registration_sorry,registration_clicked_twice,registration_help,kind_regards,
+		v_verify_before_create,v_verify_invitation_before_create,v_verify_before_update,v_really_wish_to_delete,v_edit_your_account,
+		v_dear,hello,v_notification,v_registration_created,v_registration_created_subject,v_registration_created_message1,v_registration_created_message2,
+		v_please_confirm,v_your_account_was_created,v_your_account_was_created_nomail,v_follow_instructions1,v_follow_instructions2,v_invitation_confirm,
+		v_invitation_account_was_created,v_invitation_instructions1,
+		v_registration_initiated,v_registration_initiated_subject,v_registration_initiated_message1,v_registration_initiated_message2,
+		v_registration_invited,v_registration_invited_subject,v_registration_invited_message1,v_registration_invited_message2,
+		v_registration_confirmed,v_registration_confirmed_subject,v_registration_confirmed_message1,v_registration_confirmed_message2,
+		v_registration_cancelled,v_registration_cancelled_subject,v_registration_cancelled_message1,v_registration_cancelled_message2,
+		v_registration_updated,v_registration_updated_subject,v_registration_updated_message1,v_registration_deleted,v_registration_deleted_subject,
+		v_registration_deleted_message1,v_registration_deleted_message2,v_registration_updated_subject,v_registration_updated_message1,v_registration_deleted,
+		v_sending_infomail,v_sending_infomail_message1,v_sending_infomail_message2,v_infomail_subject,v_infomail_reason,v_infomail_message1,v_infomail_message2,
+		v_infomail_norecord_subject,v_infomail_norecord_message1,v_infomail_norecord_message2';
+
+		$mainObj = &t3lib_div::getUserObj('&tx_srfeuserregister_control_main');
+		$mainObj->cObj = &$this->cObj;
+		$content =
+			&$mainObj->main(
+				$content,
+				$conf,
+				$this,
+				'tt_address',
+				$adminFieldList,
+				$buttonLabelsList,
+				$otherLabelsList
+			);
+		return $content;
+	}
+}
+
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/sr_email_subscribe/pi1/class.tx_sremailsubscribe_pi1_base.php']) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/sr_email_subscribe/pi1/class.tx_sremailsubscribe_pi1_base.php']);
+}
+
+?>
