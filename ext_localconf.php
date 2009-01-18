3b5c37ceca610a3a36dda014f6ebfd1d35adbce8
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-18 15:16:35 +0000

    modifications for usage with sr_feuser_register 2.5.20 support for patch 1822
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe/trunk@15780 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -29,6 +29,19 @@ if (!defined ('DIV2007_EXTkey')) {
 	define('DIV2007_EXTkey','div2007');
 }
 
+if (!defined ('TT_ADDRESS_EXTkey')) {
+	define('TT_ADDRESS_EXTkey','tt_address');
+}
+
+if (!defined ('PARTNER_EXTkey')) {
+	define('PARTNER_EXTkey','partner');
+}
+
+if (!defined ('PARTY_EXTkey')) {
+	define('PARTY_EXTkey','tx_party');
+}
+
+
 $bPhp5 = version_compare(phpversion(), '5.0.0', '>=');
 
 t3lib_extMgm::addPItoST43(SR_EMAIL_SUBSCRIBE_EXTkey, 'pi1/class.tx_sremailsubscribe_pi1.php', '_pi1', 'list_type', 0);
@@ -37,6 +50,7 @@ $_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we 
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['imageFolder'] = $_EXTCONF['imageFolder'];
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['useFlexforms'] = $_EXTCONF['useFlexforms'];
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['addressTable'] = $_EXTCONF['addressTable'];
 
 if (t3lib_extMgm::isLoaded(DIV2007_EXTkey))	{
 	if (!defined ('PATH_BE_div2007')) {
@@ -57,6 +71,48 @@ if (t3lib_extMgm::isLoaded(DIV2007_EXTkey) && $bPhp5) {
 if ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['useFlexforms'] && $bPhp5)	{
 	// replace the output of the former CODE field with the flexform
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info'][SR_EMAIL_SUBSCRIBE_EXTkey.'_pi1'][] = 'EXT:'.SR_EMAIL_SUBSCRIBE_EXTkey.'/hooks/class.tx_sremailsubscribe_hooks_cms.php:&tx_sremailsubscribe_hooks_cms->pmDrawItem';
+}
+
+$addressTable = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['addressTable'];
+
+if (!$addressTable)	{
+	if (t3lib_extMgm::isLoaded(PARTY_EXTkey))	{
+		$addressTable = 'tx_wecpeople_addresses';
+	} else if (t3lib_extMgm::isLoaded(PARTNER_EXTkey))	{
+		$addressTable = 'tx_partner_main';
+	} else if (t3lib_extMgm::isLoaded(TT_ADDRESS_EXTkey)) {
+		$addressTable = 'tt_address';
+	} else {
+		$addressTable = 'fe_users';
+	}
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['addressTable'] = $addressTable;
+
+if ($_EXTCONF['usePatch1822'] && ($addressTable == 'tt_address') &&
+!defined($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['addTables'][$addressTable]['MENU'])) {
+	$tableArray = array($addressTable);
+	foreach ($tableArray as $theTable)	{
+		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['LLFile'][$theTable] = 'EXT:'.SR_EMAIL_SUBSCRIBE_EXTkey.'/locallang.xml';
+	}
+
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cms']['db_layout']['addTables'][$addressTable] = array (
+		'default' => array(
+			'MENU' => 'm_default',
+			'fList' =>  'name,title,address,zip,city,country,gender,image,uid',
+			'icon' => TRUE
+		),
+		'ext' => array (
+			'MENU' => 'm_ext',
+			'fList' =>  'name,description,email,phone,mobile,fax,www,birthday',
+			'icon' => TRUE
+		),
+		'company' => array (
+			'MENU' => 'm_company',
+			'fList' =>  'name,city,company,building,room,addressgroup',
+			'icon' => TRUE
+		),
+	);
 }
 
 ?>
