ddf90a9f29c7251c5ab65c262b8ac311e8c97af0
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-17 18:02:50 +0000

    version 1.2.9 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe/trunk@24602 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sr_email_subscribe"
 #
-# Auto generated 13-05-2009 14:09
+# Auto generated 17-09-2009 20:02
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'An email newsletter and address subscription variant of the Frontend User Registration.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.2.8',
+	'version' => '1.2.9',
 	'dependencies' => 'static_info_tables,sr_feuser_register,div2007',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,10 +35,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'static_info_tables' => '2.0.5-',
-			'sr_feuser_register' => '2.5.22-',
+			'sr_feuser_register' => '2.5.23-',
 			'php' => '4.1.0-0.0.0',
 			'typo3' => '4.0-0.0.0',
-			'div2007' => '0.1.14-',
+			'div2007' => '0.2.2-',
 		),
 		'conflicts' => array(
 		),
@@ -47,7 +47,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'patch1822' => '0.0.3-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"452a";s:16:"contributors.txt";s:4:"f10a";s:21:"ext_conf_template.txt";s:4:"eb94";s:12:"ext_icon.gif";s:4:"8d58";s:17:"ext_localconf.php";s:4:"7588";s:14:"ext_tables.php";s:4:"f45a";s:14:"ext_tables.sql";s:4:"81cc";s:13:"locallang.xml";s:4:"5ee3";s:16:"locallang_db.xml";s:4:"a9ae";s:14:"doc/manual.sxw";s:4:"33f2";s:37:"pi1/class.tx_sremailsubscribe_pi1.php";s:4:"361b";s:42:"pi1/class.tx_sremailsubscribe_pi1_base.php";s:4:"89a0";s:23:"pi1/flexform_ds_pi1.xml";s:4:"605f";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.xml";s:4:"06b5";s:41:"pi1/tx_sremailsubcribe_htmlmail_xhtml.css";s:4:"01ea";s:36:"pi1/tx_sremailsubscribe_htmlmail.css";s:4:"c0df";s:49:"pi1/tx_sremailsubscribe_pi1_captcha_css_tmpl.html";s:4:"2a47";s:41:"pi1/tx_sremailsubscribe_pi1_css_tmpl.html";s:4:"6d90";s:38:"pi1/tx_sremailsubscribe_pi1_sample.txt";s:4:"2954";s:37:"pi1/tx_sremailsubscribe_pi1_tmpl.tmpl";s:4:"4430";s:45:"hooks/class.tx_sremailsubscribe_hooks_cms.php";s:4:"b51e";s:31:"static/css_styled/constants.txt";s:4:"caa0";s:27:"static/css_styled/setup.txt";s:4:"d921";s:30:"static/old_style/constants.txt";s:4:"909b";s:30:"static/old_style/editorcfg.txt";s:4:"e422";s:26:"static/old_style/setup.txt";s:4:"e014";}',
+	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"5547";s:16:"contributors.txt";s:4:"f10a";s:21:"ext_conf_template.txt";s:4:"eb94";s:12:"ext_icon.gif";s:4:"8d58";s:17:"ext_localconf.php";s:4:"7588";s:14:"ext_tables.php";s:4:"e9aa";s:14:"ext_tables.sql";s:4:"81cc";s:13:"locallang.xml";s:4:"5ee3";s:16:"locallang_db.xml";s:4:"a9ae";s:14:"doc/manual.sxw";s:4:"a95a";s:37:"pi1/class.tx_sremailsubscribe_pi1.php";s:4:"361b";s:42:"pi1/class.tx_sremailsubscribe_pi1_base.php";s:4:"8246";s:23:"pi1/flexform_ds_pi1.xml";s:4:"605f";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.xml";s:4:"06b5";s:41:"pi1/tx_sremailsubcribe_htmlmail_xhtml.css";s:4:"01ea";s:36:"pi1/tx_sremailsubscribe_htmlmail.css";s:4:"c0df";s:49:"pi1/tx_sremailsubscribe_pi1_captcha_css_tmpl.html";s:4:"2a47";s:41:"pi1/tx_sremailsubscribe_pi1_css_tmpl.html";s:4:"6d90";s:38:"pi1/tx_sremailsubscribe_pi1_sample.txt";s:4:"2954";s:37:"pi1/tx_sremailsubscribe_pi1_tmpl.tmpl";s:4:"4430";s:45:"hooks/class.tx_sremailsubscribe_hooks_cms.php";s:4:"b51e";s:31:"static/css_styled/constants.txt";s:4:"e1eb";s:27:"static/css_styled/setup.txt";s:4:"8fac";s:30:"static/old_style/constants.txt";s:4:"11e0";s:30:"static/old_style/editorcfg.txt";s:4:"e422";s:26:"static/old_style/setup.txt";s:4:"9352";}',
 	'suggests' => array(
 	),
 );
