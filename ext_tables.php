f808ac9c6acc30d5801896f1b1ab4d3cbb611e28
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-14 01:15:05 +0000

    Add feature #36006: Honour tt_address extension configuration settings
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe/trunk@60672 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,8 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 t3lib_extMgm::addStaticFile(SR_EMAIL_SUBSCRIBE_EXTkey, 'static/css_styled/', 'Email Address Subscription CSS-styled');
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][SR_EMAIL_SUBSCRIBE_EXTkey.'_pi1']='layout,select_key';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][SR_EMAIL_SUBSCRIBE_EXTkey.'_pi1']='pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][SR_EMAIL_SUBSCRIBE_EXTkey.'_pi1']='layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][SR_EMAIL_SUBSCRIBE_EXTkey.'_pi1']='pi_flexform';
 t3lib_extMgm::addPiFlexFormValue(SR_EMAIL_SUBSCRIBE_EXTkey . '_pi1', 'FILE:EXT:' . SR_EMAIL_SUBSCRIBE_EXTkey . '/pi1/flexform_ds_pi1.xml');
 
 t3lib_extMgm::addPlugin(Array('LLL:EXT:' . SR_EMAIL_SUBSCRIBE_EXTkey . '/locallang_db.xml:tt_content.email_subscribe', SR_EMAIL_SUBSCRIBE_EXTkey . '_pi1'),'list_type');
@@ -15,17 +15,16 @@ $addressTable = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey
 if ($addressTable == 'tt_address') {
 
 	/**
-	* Setting up country, country subdivision, preferred language, first_name and last_name in tt_address table
+	* Setting up country, country subdivision, preferred language in tt_address table
 	* Adjusting some maximum lengths to the values as corresponding fields in fe_users as set by extension sr_feuser_register
 	*/
-
 	t3lib_div::loadTCA('tt_address');
 
 	if (
 		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['useImageFolder'] &&
 		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['imageFolder'] != ''
 	) {
-		$TCA['tt_address']['columns']['image']['config']['uploadfolder'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['imageFolder'];
+		$GLOBALS['TCA']['tt_address']['columns']['image']['config']['uploadfolder'] = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][SR_EMAIL_SUBSCRIBE_EXTkey]['imageFolder'];
 	}
 
 	t3lib_extMgm::addTCAcolumns('tt_address', Array(
@@ -85,20 +84,19 @@ if ($addressTable == 'tt_address') {
 		),
 	));
 
-	$TCA['tt_address']['interface']['showRecordFieldList'] = str_replace('country', 'country,zone,static_info_country,language', $TCA['tt_address']['interface']['showRecordFieldList']);
+	$GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList'] = preg_replace('/(^|,)\s*country\s*(,|$)/', '$1zone,static_info_country,country,language$2', $GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList']);
+	$GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList'] = preg_replace('/(^|,)\s*title\s*(,|$)/', '$1date_of_birth,title$2', $GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList']);
+	$GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList'] = preg_replace('/(^|,)\s*www\s*(,|$)/', '$1www,comments$2', $GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList']);
+	
+	$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'] = preg_replace('/(^|,)\s*country\s*(,|$)/', '$1zone,static_info_country,country,language,comments$2', $GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList']);
+	$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'] .= ',date_of_birth';
 
-	$TCA['tt_address']['interface']['showRecordFieldList'] = str_replace('title', 'date_of_birth,title', $TCA['tt_address']['interface']['showRecordFieldList']);
-	$TCA['tt_address']['interface']['showRecordFieldList'] = str_replace('www', 'www,comments', $TCA['tt_address']['interface']['showRecordFieldList']);
-
-	$TCA['tt_address']['feInterface']['fe_admin_fieldList'] = str_replace('country', 'zone,static_info_country,country,language,comments', $TCA['tt_address']['feInterface']['fe_admin_fieldList']);
-	$TCA['tt_address']['feInterface']['fe_admin_fieldList'] .= ',date_of_birth';
-
-	if (strstr($TCA['tt_address']['feInterface']['fe_admin_fieldList'], 'image') === FALSE)	{
-		$TCA['tt_address']['feInterface']['fe_admin_fieldList'] .= ',image';
+	if (strstr($GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'], 'image') === FALSE) {
+		$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'] .= ',image';
 	}
 
 	t3lib_extMgm::addToAllTCAtypes('tt_address', 'comments');
-	$TCA['tt_address']['palettes']['3']['showitem'] = str_replace('country', 'zone,static_info_country,country,language', $TCA['tt_address']['palettes']['3']['showitem']);
+	$GLOBALS['TCA']['tt_address']['palettes']['3']['showitem'] = preg_replace('/(^|,)\s*country\s*(,|$)/', '$1zone,static_info_country,country,language$2', $GLOBALS['TCA']['tt_address']['palettes']['3']['showitem']);
 
 		// tt_address modified
 	if (!t3lib_extMgm::isLoaded('direct_mail')) {
@@ -113,7 +111,7 @@ if ($addressTable == 'tt_address') {
 		);
 		t3lib_extMgm::addTCAcolumns('tt_address',$tempCols);
 		t3lib_extMgm::addToAllTCATypes('tt_address','--div--;Direct mail,module_sys_dmail_html;;;;1-1-1');
-		$TCA['tt_address']['feInterface']['fe_admin_fieldList'].=',module_sys_dmail_html';
+		$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'].=',module_sys_dmail_html';
 	}
 }
 
