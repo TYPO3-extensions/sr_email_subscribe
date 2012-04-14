68424059bfd721df104df0ab330f03d9bd85947f
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-14 18:39:42 +0000

    Update extensions dependencies, require TYPO3 4.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe/trunk@60716 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,9 +34,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.4.0-4.7.99',
 			'sr_feuser_register' => '3.0.0-',
-			'tt_address' => '2.1.0-',
+			'tt_address' => '2.2.0-',
 			'static_info_tables' => '2.3.0-',
 			'div2007' => '0.8.2-',
 		),
