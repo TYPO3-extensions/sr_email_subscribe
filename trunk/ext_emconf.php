3c00080debc37949264476f3fdf806a67f06bb3d
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-19 23:31:00 +0000

    Require div2007 0.9.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe@60907 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sr_email_subscribe".
 #
-# Auto generated 26-06-2010 23:42
+# Auto generated 19-04-2012 19:29
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'sr_feuser_register' => '3.0.0-',
 			'tt_address' => '2.2.0-',
 			'static_info_tables' => '2.3.0-',
-			'div2007' => '0.8.2-',
+			'div2007' => '0.9.0-',
 		),
 		'conflicts' => array(
 			'germandates' => '0.0.0-1.0.1',
@@ -49,7 +49,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'sr_freecap' => '1.5.3-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"e7bd";s:16:"contributors.txt";s:4:"f10a";s:21:"ext_conf_template.txt";s:4:"eb94";s:12:"ext_icon.gif";s:4:"8d58";s:17:"ext_localconf.php";s:4:"7588";s:14:"ext_tables.php";s:4:"e9aa";s:14:"ext_tables.sql";s:4:"81cc";s:13:"locallang.xml";s:4:"5ee3";s:16:"locallang_db.xml";s:4:"a9ae";s:14:"doc/manual.sxw";s:4:"6018";s:45:"hooks/class.tx_sremailsubscribe_hooks_cms.php";s:4:"b51e";s:37:"pi1/class.tx_sremailsubscribe_pi1.php";s:4:"c4b4";s:42:"pi1/class.tx_sremailsubscribe_pi1_base.php";s:4:"69f9";s:23:"pi1/flexform_ds_pi1.xml";s:4:"605f";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.xml";s:4:"5092";s:41:"pi1/tx_sremailsubcribe_htmlmail_xhtml.css";s:4:"01ea";s:36:"pi1/tx_sremailsubscribe_htmlmail.css";s:4:"c0df";s:49:"pi1/tx_sremailsubscribe_pi1_captcha_css_tmpl.html";s:4:"2a47";s:41:"pi1/tx_sremailsubscribe_pi1_css_tmpl.html";s:4:"eaa4";s:38:"pi1/tx_sremailsubscribe_pi1_sample.txt";s:4:"2954";s:37:"pi1/tx_sremailsubscribe_pi1_tmpl.tmpl";s:4:"6888";s:31:"static/css_styled/constants.txt";s:4:"6076";s:27:"static/css_styled/setup.txt";s:4:"eef5";s:30:"static/old_style/constants.txt";s:4:"66f0";s:30:"static/old_style/editorcfg.txt";s:4:"e422";s:26:"static/old_style/setup.txt";s:4:"a52d";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"8960";s:16:"contributors.txt";s:4:"ff50";s:16:"ext_autoload.php";s:4:"fd00";s:21:"ext_conf_template.txt";s:4:"fcd3";s:12:"ext_icon.gif";s:4:"8d58";s:17:"ext_localconf.php";s:4:"4824";s:14:"ext_tables.php";s:4:"d53f";s:14:"ext_tables.sql";s:4:"81cc";s:13:"locallang.xml";s:4:"5ee3";s:16:"locallang_db.xml";s:4:"a9ae";s:14:"doc/manual.sxw";s:4:"5cd2";s:45:"hooks/class.tx_sremailsubscribe_hooks_cms.php";s:4:"b51e";s:37:"pi1/class.tx_sremailsubscribe_pi1.php";s:4:"e7ca";s:42:"pi1/class.tx_sremailsubscribe_pi1_base.php";s:4:"98fb";s:23:"pi1/flexform_ds_pi1.xml";s:4:"5230";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.xml";s:4:"0c99";s:41:"pi1/tx_sremailsubcribe_htmlmail_xhtml.css";s:4:"01ea";s:41:"pi1/tx_sremailsubscribe_pi1_css_tmpl.html";s:4:"f423";s:38:"pi1/tx_sremailsubscribe_pi1_sample.txt";s:4:"2954";s:31:"static/css_styled/constants.txt";s:4:"a1a5";s:27:"static/css_styled/setup.txt";s:4:"9cd6";}',
 	'suggests' => array(
 	),
 );
