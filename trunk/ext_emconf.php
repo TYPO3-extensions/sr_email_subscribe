380d683a1e54d6bb3c2e955f9afe811210f1cc9e
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-23 17:04:30 +0000

    Update version to 4.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe@89972 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Email Address Subscription',
 	'description' => 'An email newsletter and address subscription variant of the Front End User Registration.',
 	'category' => 'plugin',
-	'version' => '2.1.0',
+	'version' => '4.0.0',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.2.0-6.2.99',
-			'sr_feuser_register' => '3.1.0-',
+			'sr_feuser_register' => '4.0.0-4.99.99',
 			'tt_address' => '2.2.0-',
 			'static_info_tables' => '6.3.1-6.3.99'
 		),
