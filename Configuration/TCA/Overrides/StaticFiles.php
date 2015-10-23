b0f82cb3f4afa8f42e6bc40122aca055341b5f78
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-23 17:16:26 +0000

    Follow-up to Resolves #70982: [!!!] Move default style sheets to Resources directory and add static template
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe/trunk@89974 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TCA/Overrides/StaticFiles.php
+++ Configuration/TCA/Overrides/StaticFiles.php
@@ -2,4 +2,4 @@
 defined('TYPO3_MODE') or die();
 // Configure extension static template
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sr_email_subscribe', 'Configuration/TypoScript/PluginSetup', 'Email Address Subscription Setup');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sr_email_subscribe', 'Configuration/TypoScript/PluginSetup', 'Email Address Subscription CSS Styles');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sr_email_subscribe', 'Configuration/TypoScript/DefaultStyles', 'Email Address Subscription CSS Styles');
