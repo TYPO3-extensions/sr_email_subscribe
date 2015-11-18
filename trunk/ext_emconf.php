b89ca5add452c56beb32f79f3f05b75d9a2e0878
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-18 16:54:54 +0000

    Resolves #71661: [!!!] Refactor, streamline and cleanup all scripts: all API changed
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_email_subscribe@90237 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -19,7 +19,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => 
 	array(
 		'depends' => array(
-			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.2.0-6.2.99',
 			'sr_feuser_register' => '4.0.0-4.99.99',
 			'tt_address' => '2.2.0-',
