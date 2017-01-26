adaaa142151fd91fb2a5094acf80dcc15c328471
Stanislas Rolland <typo3@sjbr.ca>
2017-01-26 15:16:30 -0500

    [TASK] Update version to 5.0.0
    Change-Id: I59d56e0c10c7743a8480b7af3292b410d2d698f5
    Reviewed-on: https://review.typo3.org/51440
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Email Address Subscription',
 	'description' => 'An email newsletter and address subscription variant of the Front End User Registration.',
 	'category' => 'plugin',
-	'version' => '4.0.2',
+	'version' => '5.0.0',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'createDirs' => '',
@@ -19,10 +19,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => 
 	array(
 		'depends' => array(
-			'typo3' => '6.2.4-7.6.99',
-			'sr_feuser_register' => '4.0.2-4.0.99',
-			'tt_address' => '3.0.0-3.0.99',
-			'static_info_tables' => '6.3.9-6.3.99'
+			'typo3' => '7.6.0-8.99.99',
+			'sr_feuser_register' => '5.0.0-5.0.99',
+			'tt_address' => '3.2.1-3.2.99',
+			'static_info_tables' => '6.4.2-6.4.99'
 		),
 		'conflicts' => array(
 			'germandates' => '0.0.0-99.99.99',
@@ -30,8 +30,8 @@ $EM_CONF[$_EXTKEY] = array(
 			'patch1822' => '0.0.0-99.99.99'
 		),
 		'suggests' => array(
-			'sr_freecap' => '2.3.0-2.3.99',
-			'direct_mail' => '4.1.0-5.9.99'
+			'sr_freecap' => '2.4.0-2.4.99',
+			'direct_mail' => '5.1.0-5.9.99'
 		)
 	)
 );
